0687258f27ff68da972e52a12380124947febe3a
Ethan Sagin <ethansagin@gmail.com>
2019-06-01 22:27:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      binding.pry
+      #binding.pry
       if !item
         resp.write "Item not found"
         resp.status = 400
